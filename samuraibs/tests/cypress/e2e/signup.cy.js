c882e1ee1901ccdd70cc3f67cc218c85b8aed91a
André Luiz Kreutzer de Jesus <and.kreutzer@gmail.com>
2024-07-27 20:44:16 -0300

    troubleshoot: visualize duplicate user toast

--- samuraibs/tests/cypress/e2e/signup.cy.js
+++ samuraibs/tests/cypress/e2e/signup.cy.js
@@ -7,5 +7,15 @@ it("deve cadastrar um novo usuário", () => {
 
   cy.get('input[placeholder="Nome"]').type(name)
   cy.get('input[placeholder="E-mail"]').type(email)
-  cy.get('input[placeholder="Senha"]').type(password)
+  cy.get('input[placeholder="Senha"]').type(password) 
+
+  cy.contains('button', 'Cadastrar').click()
+
+  cy.get('.toast')
+    .should('be.visible')
+    .find('p')
+    .should('have.text', 'Agora você pode fazer seu login no Samurai Barbershop!')
+
+  cy.wait(1000)
+  cy.get('body')
 });
