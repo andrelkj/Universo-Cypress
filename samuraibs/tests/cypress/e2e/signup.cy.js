ddc8a98ff323fb9f00b2aee3e1ef7f9a1e2a22c8
André Luiz Kreutzer de Jesus <and.kreutzer@gmail.com>
2024-07-27 20:48:45 -0300

    setup: install fakerjs.dev

--- samuraibs/tests/cypress/e2e/signup.cy.js
+++ samuraibs/tests/cypress/e2e/signup.cy.js
@@ -5,17 +5,17 @@ it("deve cadastrar um novo usuário", () => {
 
   cy.visit("/signup");
 
-  cy.get('input[placeholder="Nome"]').type(name)
-  cy.get('input[placeholder="E-mail"]').type(email)
-  cy.get('input[placeholder="Senha"]').type(password) 
+  cy.get('input[placeholder="Nome"]').type(name);
+  cy.get('input[placeholder="E-mail"]').type(email);
+  cy.get('input[placeholder="Senha"]').type(password);
 
-  cy.contains('button', 'Cadastrar').click()
+  cy.contains("button", "Cadastrar").click();
 
-  cy.get('.toast')
-    .should('be.visible')
-    .find('p')
-    .should('have.text', 'Agora você pode fazer seu login no Samurai Barbershop!')
-
-  cy.wait(1000)
-  cy.get('body')
+  cy.get(".toast")
+    .should("be.visible")
+    .find("p")
+    .should(
+      "have.text",
+      "Agora você pode fazer seu login no Samurai Barbershop!"
+    );
 });
