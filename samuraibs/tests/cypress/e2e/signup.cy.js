c24cd0b8377e60aa686f2ea0ab562f54580ec017
André Luiz Kreutzer de Jesus <and.kreutzer@gmail.com>
2024-07-27 23:43:52 -0300

    dev: create validation for duplicated emails message

--- samuraibs/tests/cypress/e2e/signup.cy.js
+++ samuraibs/tests/cypress/e2e/signup.cy.js
@@ -3,10 +3,9 @@ it("deve cadastrar um novo usuário", () => {
   const email = "test@gmail.com";
   const password = "pwd123";
 
-  cy.task('removeUser', email)
-    .then(function(result){
-      console.log(result);
-    })
+  cy.task("removeUser", email).then(function (result) {
+    console.log(result);
+  });
 
   cy.visit("/signup");
 
@@ -14,13 +13,7 @@ it("deve cadastrar um novo usuário", () => {
   cy.get('input[placeholder="E-mail"]').type(email);
   cy.get('input[placeholder="Senha"]').type(password);
 
-  // cy.intercept('POST', '/users', {
-  //   statusCode: 200
-  // }).as('postUser')
-
   cy.contains("button", "Cadastrar").click();
-
-  // cy.wait('@postUser')
 
   cy.get(".toast")
     .should("be.visible")
@@ -29,4 +22,23 @@ it("deve cadastrar um novo usuário", () => {
       "have.text",
       "Agora você pode fazer seu login no Samurai Barbershop!"
     );
+});
+
+it("deve exibir email já cadastrado", () => {
+  const name = "Test User";
+  const email = "test@gmail.com";
+  const password = "pwd123";
+
+  cy.visit("/signup");
+
+  cy.get('input[placeholder="Nome"]').type(name);
+  cy.get('input[placeholder="E-mail"]').type(email);
+  cy.get('input[placeholder="Senha"]').type(password);
+
+  cy.contains("button", "Cadastrar").click();
+
+  cy.get(".toast")
+    .should("be.visible")
+    .find("p")
+    .should("have.text", "Email já cadastrado para outro usuário.");
 });
