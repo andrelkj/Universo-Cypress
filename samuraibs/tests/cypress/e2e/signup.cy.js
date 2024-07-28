5fc24a6aae856be1363cfbf8d5328f329da74b1b
André Luiz Kreutzer de Jesus <and.kreutzer@gmail.com>
2024-07-27 23:58:38 -0300

    dev: create user dictionary to store all user data

--- samuraibs/tests/cypress/e2e/signup.cy.js
+++ samuraibs/tests/cypress/e2e/signup.cy.js
@@ -1,18 +1,20 @@
 describe("cadastro", () => {
-  it("deve cadastrar um novo usuário", () => {
-    const name = "Test User";
-    const email = "test@gmail.com";
-    const password = "pwd123";
+  const user = {
+    name: "Test User",
+    email: "test@gmail.com",
+    password: "pwd123",
+  };
 
-    cy.task("removeUser", email).then(function (result) {
+  it("deve cadastrar um novo usuário", () => {
+    cy.task("removeUser", user.email).then(function (result) {
       console.log(result);
     });
 
     cy.visit("/signup");
 
-    cy.get('input[placeholder="Nome"]').type(name);
-    cy.get('input[placeholder="E-mail"]').type(email);
-    cy.get('input[placeholder="Senha"]').type(password);
+    cy.get('input[placeholder="Nome"]').type(user.name);
+    cy.get('input[placeholder="E-mail"]').type(user.email);
+    cy.get('input[placeholder="Senha"]').type(user.password);
 
     cy.contains("button", "Cadastrar").click();
 
@@ -26,15 +28,11 @@ describe("cadastro", () => {
   });
 
   it("deve exibir email já cadastrado", () => {
-    const name = "Test User";
-    const email = "test@gmail.com";
-    const password = "pwd123";
-
     cy.visit("/signup");
 
-    cy.get('input[placeholder="Nome"]').type(name);
-    cy.get('input[placeholder="E-mail"]').type(email);
-    cy.get('input[placeholder="Senha"]').type(password);
+    cy.get('input[placeholder="Nome"]').type(user.name);
+    cy.get('input[placeholder="E-mail"]').type(user.email);
+    cy.get('input[placeholder="Senha"]').type(user.password);
 
     cy.contains("button", "Cadastrar").click();
 
