1564072f1da83b8f9b0790f25bcf9bbc55f19de8
André Luiz Kreutzer de Jesus <and.kreutzer@gmail.com>
2024-07-27 22:10:14 -0300

    dev: create an intercet to mock a 200 status post for a successful registry

--- samuraibs/tests/cypress/e2e/signup.cy.js
+++ samuraibs/tests/cypress/e2e/signup.cy.js
@@ -1,8 +1,6 @@
-import { faker } from "@faker-js/faker";
-
 it("deve cadastrar um novo usuário", () => {
   const name = "Test User";
-  const email = faker.internet.email();
+  const email = "test@gmail.com";
   const password = "pwd123";
 
   cy.visit("/signup");
@@ -11,7 +9,13 @@ it("deve cadastrar um novo usuário", () => {
   cy.get('input[placeholder="E-mail"]').type(email);
   cy.get('input[placeholder="Senha"]').type(password);
 
+  cy.intercept('POST', '/users', {
+    statusCode: 200
+  }).as('postUser')
+
   cy.contains("button", "Cadastrar").click();
+
+  cy.wait('@postUser')
 
   cy.get(".toast")
     .should("be.visible")
