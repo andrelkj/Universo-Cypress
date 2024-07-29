7ea33ef049fb9285c6410511c8d89cb1673116b6
André Luiz Kreutzer de Jesus <and.kreutzer@gmail.com>
2024-07-29 19:15:32 -0300

    update: create dynamic data for the login validation

--- samuraibs/tests/cypress/e2e/login.cy.js
+++ samuraibs/tests/cypress/e2e/login.cy.js
@@ -1,5 +1,5 @@
 import loginPage from "../support/pages/login";
-import dashPage from "../support/pages/dashboard"
+import dashPage from "../support/pages/dashboard";
 
 describe("login", () => {
   context("quando o usuário é muito bom", () => {
@@ -7,14 +7,27 @@ describe("login", () => {
       name: "Test User",
       email: "test@gmail.com",
       password: "pwd123",
+      is_provider: true,
     };
+
+    before(() => {
+      cy.task("removeUser", user.email).then(function (result) {
+        console.log(result);
+      });
+
+      cy.request("POST", "http://localhost:3333/users", user).then(function (
+        response
+      ) {
+        expect(response.status).to.eq(200);
+      });
+    });
 
     it("deve logar com sucesso", () => {
       loginPage.go();
-      loginPage.form(user)
-      loginPage.submit()
+      loginPage.form(user);
+      loginPage.submit();
 
-      dashPage.header.userLoggedIn(user.name)
+      dashPage.header.userLoggedIn(user.name);
     });
   });
-})
+});
