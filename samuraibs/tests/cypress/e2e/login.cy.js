779a40e8c1204e04dfd768d2590ffbad194d347e
André Luiz Kreutzer de Jesus <and.kreutzer@gmail.com>
2024-07-29 19:46:43 -0300

    dev: create invalid password login

--- samuraibs/tests/cypress/e2e/login.cy.js
+++ samuraibs/tests/cypress/e2e/login.cy.js
@@ -22,4 +22,25 @@ describe("login", () => {
       dashPage.header.userLoggedIn(user.name);
     });
   });
+
+  context.only("quando o usuário é bom mais a senha está incorreta", () => {
+    let user = {
+      name: "Test User 2",
+      email: "test2@gmail.com",
+      password: "pwd123",
+      is_provider: true,
+    };
+
+    before(() => {
+      cy.postUser(user).then(() => {
+        user.password = "abc123";
+      });
+    });
+
+    it("deve notificar erro de credenciais", () => {
+      loginPage.go();
+      loginPage.form(user);
+      loginPage.submit();
+    });
+  });
 });
