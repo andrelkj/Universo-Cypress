e8797c562b5d86d6d34946069a9f78da8fde8503
André Luiz Kreutzer de Jesus <and.kreutzer@gmail.com>
2024-07-29 19:24:23 -0300

    Improve: create custom command to remove and post a new user

--- samuraibs/tests/cypress/e2e/login.cy.js
+++ samuraibs/tests/cypress/e2e/login.cy.js
@@ -11,15 +11,7 @@ describe("login", () => {
     };
 
     before(() => {
-      cy.task("removeUser", user.email).then(function (result) {
-        console.log(result);
-      });
-
-      cy.request("POST", "http://localhost:3333/users", user).then(function (
-        response
-      ) {
-        expect(response.status).to.eq(200);
-      });
+      cy.postUser(user);
     });
 
     it("deve logar com sucesso", () => {
