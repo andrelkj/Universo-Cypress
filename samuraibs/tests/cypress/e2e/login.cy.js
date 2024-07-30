01c61741e486bd57e1b3c38a8acb9f11c32d38c9
André Luiz Kreutzer de Jesus <and.kreutzer@gmail.com>
2024-07-29 22:11:34 -0300

    dev: add validation to invalid email formats

--- samuraibs/tests/cypress/e2e/login.cy.js
+++ samuraibs/tests/cypress/e2e/login.cy.js
@@ -23,7 +23,7 @@ describe("login", () => {
     });
   });
 
-  context.only("quando o usuário é bom mais a senha está incorreta", () => {
+  context("quando o usuário é bom mais a senha está incorreta", () => {
     let user = {
       name: "Test User 2",
       email: "test2@gmail.com",
@@ -41,6 +41,33 @@ describe("login", () => {
       loginPage.go();
       loginPage.form(user);
       loginPage.submit();
+
+      const message =
+        "Ocorreu um erro ao fazer login, verifique suas credenciais.";
+      loginPage.toast.shouldHaveText(message);
+    });
+  });
+
+  context("quando o formato do email é inválido", () => {
+    const emails = [
+      "test.com.br",
+      "gmail.com",
+      "@gmail.com",
+      "@",
+      "test@",
+      "111",
+      "&*^&^&",
+      "test123",
+    ];
+
+    emails.forEach((email) => {
+      it("não deve logar com o email: " + email, () => {
+        const user = { email: email, password: "pwd123" };
+        loginPage.go();
+        loginPage.form(user);
+        loginPage.submit();
+        loginPage.alertHaveText("Informe um email válido");
+      });
     });
   });
 });
