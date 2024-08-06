c7be6183c48a451a37a53e4b30202d0b50426391
André Luiz Kreutzer de Jesus <and.kreutzer@gmail.com>
2024-08-06 12:42:51 -0300

    dev: store token as a env variable

--- samuraibs/tests/cypress/e2e/dashboard.cy.js
+++ samuraibs/tests/cypress/e2e/dashboard.cy.js
@@ -18,10 +18,30 @@ describe("dashboard", function () {
     before(function () {
       cy.postUser(data.customer);
       cy.postUser(data.provider);
+
+      cy.apiLogin(data.customer);
+      cy.log("Conseguimos pegar o token " + Cypress.env("apiToken"));
     });
 
     it("o mesmo deve ser exibido no dashboard", function () {
       console.log(data);
     });
+  });
+});
+
+Cypress.Commands.add("apiLogin", function (user) {
+  const payload = {
+    email: user.email,
+    password: user.password,
+  };
+
+  cy.request({
+    method: "POST",
+    url: "http://localhost:3333/sessions",
+    body: payload,
+  }).then(function (response) {
+    expect(response.status).to.eq(200);
+    console.log(response.body.token);
+    Cypress.env("apiToken", response.body.token);
   });
 });
