8b2c0d4b20b3a730794be1b1a02f528f7eb0ff46
André Luiz Kreutzer de Jesus <and.kreutzer@gmail.com>
2024-08-06 19:49:53 -0300

    dev: create functions in the dash page to validate the calendar and the appoitment data

--- samuraibs/tests/cypress/e2e/dashboard.cy.js
+++ samuraibs/tests/cypress/e2e/dashboard.cy.js
@@ -1,5 +1,5 @@
-import login from '../support/pages/login';
 import loginPage from '../support/pages/login';
+import dashboardPage from '../support/pages/dashboard';
 
 describe('dashboard', function () {
   context('quando o cliente faz um agendamento no app mobile', function () {
@@ -16,39 +16,44 @@ describe('dashboard', function () {
         password: 'pwd123',
         is_provider: true,
       },
+      appointmentHour: '14:00',
     };
 
     before(function () {
       cy.postUser(data.provider);
       cy.postUser(data.customer);
 
-      cy.apiLogin(data.customer).then(() => {
-        cy.log('Conseguimos pegar o token ' + Cypress.env('apiToken'));
-      });
-
+      cy.apiLogin(data.customer);
       cy.setProviderId(data.provider.email);
-
-      cy.createAppointment();
+      cy.createAppointment(data.appointmentHour);
     });
 
     it('o mesmo deve ser exibido no dashboard', function () {
       loginPage.go();
       loginPage.form(data.provider);
       loginPage.submit();
+
+      dashboardPage.calendarShouldBeVisible();
+
+      const day = Cypress.env('appointmentDay');
+      dashboardPage.selectDay(day);
+
+      dashboardPage.appoitmentShouldBe(data.customer, data.appointmentHour);
     });
   });
 });
 
 import moment from 'moment';
 
-Cypress.Commands.add('createAppointment', function () {
+Cypress.Commands.add('createAppointment', function (hour) {
   // define a variable to store the tomorrow date as a number
   let now = new Date();
 
   now.setDate(now.getDate() + 1);
+  Cypress.env('appointmentDay', now.getDate());
 
   // format date using moment library
-  const date = moment(now).format('YYYY-MM-DD 14:00:00');
+  const date = moment(now).format('YYYY-MM-DD ' + hour + ':00');
 
   // define request payload
   const payload = {
--- samuraibs/tests/cypress/support/pages/dashboard/index.js
+++ samuraibs/tests/cypress/support/pages/dashboard/index.js
@@ -1,10 +1,27 @@
-import { el } from "./elements"
+import { el } from './elements';
 
-import header from "../../components/header";
+import header from '../../components/header';
 
 class DashPage {
   constructor() {
-    this.header = header
+    this.header = header;
+  }
+
+  calendarShouldBeVisible() {
+    cy.get('.DayPicker', { timeout: 7000 }).should('be.visible');
+  }
+
+  selectDay(day) {
+    const target = new RegExp('^' + day + '$', 'g');
+    cy.contains('.DayPicker-Day--available', target).click();
+  }
+
+  appoitmentShouldBe(customer, hour) {
+    cy.contains('div', customer.name)
+      .should('be.visible')
+      .parent()
+      .contains('span[class=appointment]', hour)
+      .should('be.visible');
   }
 }
 
